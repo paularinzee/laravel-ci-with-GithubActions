a8210b45c2593f7519d9be9eb494a9cf661b7557
paularinzee <arinzennajipual@gmail.com>
2023-11-27 21:18:17 +0100

    edit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,12 +11,12 @@ class ExampleTest extends TestCase
      * A basic test example.
      */
     // public function test_the_application_returns_a_successful_response(): void
-    // public function test_the_application_returns_a_successful_response(): void 
-    // {
-    //     $response = $this->get('/');
+    public function test_the_application_returns_a_successful_response(): void 
+    {
+        $response = $this->get('/');
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(200);
+    }
 
     public function a_user_can_visit_the_home_page(): void 
     {
